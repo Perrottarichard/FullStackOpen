846ffb3e154d54f4e1e3fb3ce8e06e922c1a49aa
Richard Perrotta <perrottarichard@gmail.com>
2020-05-19 16:56:53 +0700

    api working 3.6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+
+app.use(express.json());
 
 let persons = [
     {
@@ -25,7 +26,6 @@ let persons = [
     }
 ]
 
-app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -46,15 +46,55 @@ app.get('/api/persons/:id', (req, res) => {
     }
 
 })
+
+app.get('/info', (req, res) => {
+    res.send(`<h3>Phonebook has info for ${persons.length} people</h3>${Date()}`)
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    console.log(persons.filter(p => p.name === body.name).length > 0)
+    console.log(body)
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    }
+    if (persons.filter(p => p.name === body.name).length > 0) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    if (persons.filter(p => p.number === body.number).length > 0) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+    persons = persons.concat(newPerson);
+    res.json(newPerson)
+})
+
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
     res.status(204).end();
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<h3>Phonebook has info for ${persons.length} people</h3>${Date()}`)
-})
 
 const PORT = 3001
 app.listen(PORT, () => {
